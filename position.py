4c42c353841e0f70ecdd6bfb18fce22d4ba82c57
acpirience <acpirience@gmail.com>
2024-01-22 18:08:10 +0100

    invert board for position analysis if Black is to play

--- position.py
+++ position.py
@@ -14,14 +14,18 @@ class Position:
         self.board = board
         self.flags = flags
 
+        if self.flags["color"] == "b":
+            self._invert_board()
+
     def get_possible_moves(self) -> list[str]:
         pieces = []
 
-        # get all pieces of the color of which turn it is
+        # get all pieces white color
         for line in range(8):
             for col in range(8):
-                if self.board[line][col].color == self.flags["color"]:
+                if self.board[line][col].color == "w":
                     pieces.append((self.board[line][col], line, col))
+                    logger.info(f"{self.board[line][col]}, {line} {col}")
 
         for piece, line, col in pieces:
             moves = self.get_moves_for_piece(piece, line, col)
@@ -32,37 +36,55 @@ class Position:
 
         # Pawn
         if piece.piece == "P":
-            if self.flags["color"] == "w":
-                # move
-                if not self.board[line - 1][col]:  # Pawn move 1 square
-                    moves.append(
-                        f"{Position._col_to_letter(col)}{Position._line_to_board(line -1)}"
-                    )
-                    if line == 6:  # if first move can move 2 squares
-                        if not self.board[line - 2][col]:
-                            moves.append(
-                                f"{Position._col_to_letter(col)}{Position._line_to_board(line - 2)}"
-                            )
-                # capture
-                for i in [-1, 1]:
-                    if 0 <= col + i <= 7:
-                        if (
-                            self.board[line - 1][col + i]
-                            and self.board[line - 1][col + i].color == "b"
-                        ):
-                            moves.append(
-                                f"{Position._col_to_letter(col)}x{Position._col_to_letter(col + i)}{Position._line_to_board(line - 1)}"
-                            )
-                # en passant TBD
+            # move
+            if not self.board[line - 1][col]:  # Pawn move 1 square
+                moves.append(f"{Position._col_to_letter(col)}{self._line_to_board(line -1)}")
+                if line == 6:  # if first move can move 2 squares
+                    if not self.board[line - 2][col]:
+                        moves.append(
+                            f"{Position._col_to_letter(col)}{self._line_to_board(line - 2)}"
+                        )
+            # capture
+            for i in [-1, 1]:
+                if 0 <= col + i <= 7:
+                    if self.board[line - 1][col + i] and self.board[line - 1][col + i].color == "b":
+                        moves.append(
+                            f"{Position._col_to_letter(col)}x{Position._col_to_letter(col + i)}{self._line_to_board(line - 1)}"
+                        )
+            # en passant TBD
 
         if moves:
             logger.info(moves)
         return moves
 
+    def _line_to_board(self, line: int) -> int:
+        if self.flags["color"] == "w":
+            return 8 - line
+        return line + 1
+
+    def _invert_board(self) -> None:
+        new_board = []
+        for line in reversed(self.board):
+            new_line = []
+            for piece in line:
+                if piece:
+                    new_line.append(Piece(piece.piece, Position._invert_color(piece.color)))
+                else:
+                    new_line.append(Piece())
+            new_board.append(new_line)
+
+        self.board = new_board
+        for x in range(8):
+            logger.info(
+                f"{self.board[x][0]} {self.board[x][1]} {self.board[x][2]} {self.board[x][3]} {self.board[x][4]} {self.board[x][5]} {self.board[x][6]} {self.board[x][7]}"
+            )
+
+    @staticmethod
+    def _invert_color(color: str) -> str:
+        if color == "w":
+            return "b"
+        return "w"
+
     @staticmethod
     def _col_to_letter(col: int) -> str:
         return f"{chr(col + 97)}"
-
-    @staticmethod
-    def _line_to_board(line: int) -> int:
-        return 8 - line
