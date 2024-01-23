a56a881fe211860844cecccd3b2e6d9e4a1333ed
acpirience <acpirience@gmail.com>
2024-01-23 09:51:34 +0100

    Add test: test_move_pawns_mixed

--- test/test_position_get_moves_for_piece.py
+++ test/test_position_get_moves_for_piece.py
@@ -46,3 +46,23 @@ def test_move_pawns_alone(test_board: Board, test_flags: dict[str, str]) -> None
         "h3",
         "h4",
     ]
+
+
+def test_move_pawns_blocked(test_board: Board, test_flags: dict[str, str]) -> None:
+    test_board.load_board_from_FEN("8/8/8/8/8/p1p1p1p1/P1P1P1P1/8")
+
+    test_position = Position(test_board.board_content, test_flags)
+    possible_moves = test_position.get_possible_moves()
+
+    possible_moves.sort()
+    assert possible_moves == []
+
+
+def test_move_pawns_mixed(test_board: Board, test_flags: dict[str, str]) -> None:
+    test_board.load_board_from_FEN("8/8/6p1/4p3/2p5/p7/P1P1P1P1/8")
+
+    test_position = Position(test_board.board_content, test_flags)
+    possible_moves = test_position.get_possible_moves()
+
+    possible_moves.sort()
+    assert possible_moves == ["c3", "e3", "e4", "g3", "g4"]
